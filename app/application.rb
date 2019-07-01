833363637d71372b974ebf04cd310e788f6879b6
Chinedu Arinze <c.s.arinze@gmail.com>
2019-07-01 01:48:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -41,7 +41,6 @@ class Application
 
   def handle_add(item_to_add)
     if @@items.include?(item_to_add)
-      @@cart << item_to_add
       return "added #{item_to_add} to your cart"
     else
       return "We don't have that item"
